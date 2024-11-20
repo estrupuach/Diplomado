63cdf653ebd996af8ae153c816e48b17d8e0e928
Cristian Camilo Avendaño Cadavid <99198072+estrupuach@users.noreply.github.com>
2024-11-20 01:57:12 +0000

    subo tercera app

---
+++ docker/tercera-aplicacion/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import boto3
+
+app = Flask(__name__)
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+tabla = dynamodb.Table('tabla-cristian')
+
+
+@app.route('/insert', methods=['POST'])
+def index():
+    data = request.json
+    item = {
+        **data
+    }
+    tabla.put_item(Item=item)
+    return 'Se guardo exitosamente'
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=5000)
