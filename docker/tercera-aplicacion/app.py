680c7298c2718c82ffc7a41698c2e2766e373cc8
Cristian Camilo Avendaño Cadavid <99198072+estrupuach@users.noreply.github.com>
2024-11-21 00:14:35 +0000

    Se agrega microservicio de leer datos de dynamo por clave

--- docker/tercera-aplicacion/app.py
+++ docker/tercera-aplicacion/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import boto3
 
 app = Flask(__name__)
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tabla = dynamodb.Table('tabla-cristian')
 
-
+# Metodo para insertar elementos en una tabla de dynamo
 @app.route('/insert', methods=['POST'])
 def index():
     data = request.json
@@ -14,6 +14,22 @@ def index():
     }
     tabla.put_item(Item=item)
     return 'Se guardo exitosamente'
+
+#Metodo para leer elementos de una tabla de dynamo
+@app.route('/get/<clave>', methods=['GET'])
+def get_item(clave):
+    try:
+        response = tabla.get_item(Key={'clave': clave}
+
+        if 'Item' in response:
+            return jsonify(response['Item']), 200
+        else:
+            return jsonify({'message': 'No se encontro la clave'}), 404
+                
+        ) 
+    except Exception as e:
+        return jsonify({'error': str(e)}),500
+        
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=5000)
