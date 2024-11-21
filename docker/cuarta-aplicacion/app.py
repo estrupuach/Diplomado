e9157e33a1a8a6888476607977b1e3b64c47a397
Cristian Camilo Avendaño Cadavid <99198072+estrupuach@users.noreply.github.com>
2024-11-21 02:11:18 +0000

    Se agrega cuerta aplicacion 5

--- docker/cuarta-aplicacion/app.py
+++ docker/cuarta-aplicacion/app.py
@@ -1,35 +1,33 @@
-import boto3
-from flask import Flask, request, jsonify, send_file
-import os
+  import boto3 
+  from flask import Flask, request, jsonify, send_file
+  import os
 
-app = Flask(__name__)
+  app = Flask(__name__)
 
-@app.route('/voice', methods=['POST'])
-def voice():
-  try:
-    data = request.json
-    text = data.get('text')
-    voice = 'Mia'
-    output_fromat = 'mp3'
+  @app.route('/voice', methods=['POST'])
+  def voice():
+    try:
+      data = request.json
+      text = data.get('text')
+      voice = "Mia"
+      output_format = "mp3"
 
-    if not text:
-      return jsonify({'error': 'No se recibio el texto'}), 400
+      if not text:
+        return jsonify({'error': 'No se recibió el texto'}), 400
 
-    polly = boto3.client('polly', region_name='us-east-1')
+      polly = boto3.client('polly', region_name='us-east-1')
 
-    response = polly.synthesize_speech(Text=text,
-                                       OutputFormat=output_format,
-                                       VoiceId=voice)
+      response = polly.synthesize_speech(
+        Text=text,
+        OutputFormat=output_format,
+        VoiceId=voice
+      )
 
-    audio_file = "output.mp3"
-    with open(audio_file, 'wb') as f:
-      file.write(response['AudioStream'].read())
+      audio_file = "output.mp3"
+      with open(audio_file, 'wb') as file:
+        file.write(response['AudioStream'].read())
 
-    return send_file(audio_file, mimetype='audio/mpeg')
-    
-  except Exception as e:
-    return jsonify({'error': str(e)}),500
+      return send_file(audio_file, mimetype='audio/mpeg')
 
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000)
+    except Exception as e:
+      return jsonify({'error': str(e)}), 500
