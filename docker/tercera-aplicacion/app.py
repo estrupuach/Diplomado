66eefacf2c333bf4e8bf6a58aab3a5ee1e9f70a4
Cristian Camilo Avendaño Cadavid <99198072+estrupuach@users.noreply.github.com>
2024-11-23 00:17:05 +0000

    Modificacion

--- docker/tercera-aplicacion/app.py
+++ docker/tercera-aplicacion/app.py
@@ -3,33 +3,34 @@ import boto3
 
 app = Flask(__name__)
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
-tabla = dynamodb.Table('tabla-cristian')
+tabla = dynamodb.Table('tabla-juangomez')
 
-# Metodo para insertar elementos en una tabla de dynamo
+
+#Metodo para insertar elementos en una tabla de dynamodb
 @app.route('/insert', methods=['POST'])
 def index():
-    data = request.json
-    item = {
-        **data
-    }
-    tabla.put_item(Item=item)
-    return 'Se guardo exitosamente'
+  data = request.json
+  item = {**data}
 
-#Metodo para leer elementos de una tabla de dynamo
-@app.route('/get/<clave>', methods=['GET'])
-def get_item(clave):
-    try:
-        response = tabla.get_item(Key={'clave': clave}
+  tabla.put_item(Item=item)
 
-        if 'Item' in response:
-            return jsonify(response['Item']), 200
-        else:
-            return jsonify({'message': 'No se encontro la clave'}), 404
-                
-        ) 
-    except Exception as e:
-        return jsonify({'error': str(e)}),500
-        
+  return 'Se guardó exitosamente'
 
-if __name__ == '__main__':
+
+#Metodo para leer elementos de una tabla de dynamodb
+@app.route('/get/<id>', methods=['GET'])
+def get_item(id):
+  try:
+    response = tabla.get_item(Key={'id': id})
+
+    if 'Item' in response:
+      return jsonify(response['Item']), 200
+    else:
+      return jsonify({'error': 'id no existe'}), 404
+
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
+
+
+if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0', port=5000)
